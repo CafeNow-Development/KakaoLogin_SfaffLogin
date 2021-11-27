377757f92da4aaa2f07e2cbf4f6b5a62ce614c4c
김상현 <llmm030506@naver.com>
2021-11-27 18:13:54 +0900

    first commit

---
+++ src/main/java/com/login/cafenow_kakaologin_stafflogin/CafeNowKakaoLoginStaffLoginApplication.java
@@ -0,0 +1,13 @@
+package com.login.cafenow_kakaologin_stafflogin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CafeNowKakaoLoginStaffLoginApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CafeNowKakaoLoginStaffLoginApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/login/cafenow_kakaologin_stafflogin/CafeNowKakaoLoginStaffLoginApplicationTests.java
@@ -0,0 +1,13 @@
+package com.login.cafenow_kakaologin_stafflogin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CafeNowKakaoLoginStaffLoginApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
