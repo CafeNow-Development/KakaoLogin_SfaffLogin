65a872556a96cbe8ed521063041e3ac00104cbfa
김상현 <llmm030506@naver.com>
2021-12-02 20:53:47 +0900

    Details User

--- src/main/java/com/login/cafenow_kakaologin_stafflogin/common/exception/DataNotFoundException.java
+++ src/main/java/com/login/cafenow_kakaologin_stafflogin/common/exception/DataNotFoundException.java
@@ -18,5 +18,4 @@ public class DataNotFoundException extends RuntimeException {
         super(dataType.getMessage());
     }
 
-
 }
--- src/main/java/com/login/cafenow_kakaologin_stafflogin/security/authectication/MyUserDetails.java
+++ src/main/java/com/login/cafenow_kakaologin_stafflogin/security/authectication/MyUserDetails.java
@@ -1,5 +1,8 @@
 package com.login.cafenow_kakaologin_stafflogin.security.authectication;
 
+import com.login.cafenow_kakaologin_stafflogin.common.exception.DataNotFoundException;
+import com.login.cafenow_kakaologin_stafflogin.common.exception.DataNotFoundType;
+import com.login.cafenow_kakaologin_stafflogin.domain.kakao_login.repository.KakaoLoginRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -10,9 +13,10 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class MyUserDetails implements UserDetailsService {
 
+    private final KakaoLoginRepository kakaoLoginRepository;
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        return null;
+        return kakaoLoginRepository.findByEmail(username).orElseThrow(() -> new DataNotFoundException(DataNotFoundType.회원_못찾음));
     }
 }
