63c57968c6cd3372df96fb69c347ee133c504d84
김상현 <llmm030506@naver.com>
2022-01-09 14:03:17 +0900

    스텝 서비스 수정

--- src/main/java/com/login/cafenow_kakaologin_stafflogin/common/exception/badrequest/BadRequestType.java
+++ src/main/java/com/login/cafenow_kakaologin_stafflogin/common/exception/badrequest/BadRequestType.java
@@ -3,7 +3,9 @@ package com.login.cafenow_kakaologin_stafflogin.common.exception.badrequest;
 public enum BadRequestType {
 
     DEFAULT("잘못된 요청 데이터 입니다. 파라미터를 확인해 주세요."),
-    KAKAO_LOGIN_COMMUNICATION_FAIL("카카오 로그인 요청중 통신에 실패하였습니다.")
+    KAKAO_LOGIN_COMMUNICATION_FAIL("카카오 로그인 요청중 통신에 실패하였습니다."),
+    WRONG_PASSWORD("비밀번호가 일치하지 않습니다."),
+    WRONG_EMAIL("이메일이 일치하지 않습니다.")
     ;
 
     private final String message;
--- src/main/java/com/login/cafenow_kakaologin_stafflogin/domain/staff/service/StaffServiceImpl.java
+++ src/main/java/com/login/cafenow_kakaologin_stafflogin/domain/staff/service/StaffServiceImpl.java
@@ -1,5 +1,9 @@
 package com.login.cafenow_kakaologin_stafflogin.domain.staff.service;
 
+import com.login.cafenow_kakaologin_stafflogin.common.exception.badrequest.BadRequestException;
+import com.login.cafenow_kakaologin_stafflogin.common.exception.badrequest.BadRequestType;
+import com.login.cafenow_kakaologin_stafflogin.common.exception.datanotfound.DataNotFoundException;
+import com.login.cafenow_kakaologin_stafflogin.common.exception.datanotfound.DataNotFoundType;
 import com.login.cafenow_kakaologin_stafflogin.common.util.CurrentAdminUtil;
 import com.login.cafenow_kakaologin_stafflogin.domain.kakao_login.model.Admin;
 import com.login.cafenow_kakaologin_stafflogin.domain.staff.dto.StaffFindResDto;
@@ -8,21 +12,26 @@ import com.login.cafenow_kakaologin_stafflogin.domain.staff.dto.StaffLoginResDto
 import com.login.cafenow_kakaologin_stafflogin.domain.staff.dto.StaffSaveReqDto;
 import com.login.cafenow_kakaologin_stafflogin.domain.staff.model.Staff;
 import com.login.cafenow_kakaologin_stafflogin.domain.staff.repository.StaffRepository;
+import com.login.cafenow_kakaologin_stafflogin.security.jwt.JwtTokenProvider;
 import lombok.RequiredArgsConstructor;
-import org.apache.catalina.Store;
+import org.modelmapper.ModelMapper;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
-import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Service
 @Transactional(readOnly = true)
 @RequiredArgsConstructor
 public class StaffServiceImpl implements StaffService {
 
+    private final PasswordEncoder passwordEncoder;
+    private final JwtTokenProvider jwtTokenProvider;
     private final StaffRepository staffRepository;
     private final CurrentAdminUtil currentAdminUtil;
+    private final ModelMapper mapper;
 
     @Transactional
     @Override
@@ -33,21 +42,37 @@ public class StaffServiceImpl implements StaffService {
 
     @Override
     public StaffLoginResDto loginStaff(StaffLoginReqDto staffLoginReqDto) {
-        return null;
+        Staff staff = staffRepository.findByStaffEmail(staffLoginReqDto.getStaffEmail()).orElseThrow(() -> new BadRequestException(BadRequestType.WRONG_EMAIL));
+        boolean check = passwordEncoder.matches(staffLoginReqDto.getStaffPassword(), staff.getStaffPassword());
+        if(!check) {
+            throw new BadRequestException(BadRequestType.WRONG_PASSWORD);
+        }
+        StaffLoginResDto staffLoginResDto = StaffLoginResDto.builder()
+                    .staffEmail(staff.getStaffEmail())
+                    .staffName(staff.getStaffName())
+                    .accessToken(jwtTokenProvider.createTokenStaff(staff))
+                    .role(staff.getRole())
+                    .build();
+
+        return staffLoginResDto;
     }
 
     @Override
     public void deleteStaff(Long idx) {
-
+        staffRepository.deleteById(idx);
     }
 
     @Override
     public List<StaffFindResDto> findAll() {
-        return null;
+        return staffRepository.findAll().stream()
+                .map(m -> mapper.map(m, StaffFindResDto.class))
+                .collect(Collectors.toList());
     }
 
     @Override
     public StaffFindResDto findByIdx(Long idx) {
-        return null;
+        return staffRepository.findById(idx)
+                .map(m -> mapper.map(m, StaffFindResDto.class))
+                .orElseThrow(() -> new DataNotFoundException(DataNotFoundType.스텝_못찾음));
     }
 }
